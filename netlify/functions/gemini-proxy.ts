67c65b3671fc35820f873c019660c30d1f03e59f
pratrxx-art <pratrxx@gmail.com>
2026-05-12 21:50:31 +0530

    Update gemini-proxy.ts

--- netlify/functions/gemini-proxy.ts
+++ netlify/functions/gemini-proxy.ts
@@ -14,7 +14,7 @@ export const handler = async (event: any) => {
     const { prompt, systemInstruction, config } = JSON.parse(event.body);
     const ai = new GoogleGenAI({ apiKey: apiKey });
     const result = await ai.models.generateContent({
-      model: 'gemini-1.5-flash',
+      model: 'gemini-1.5-flash-8b',
       contents: [{ role: 'user', parts: [{ text: prompt }] }],
       config: {
         systemInstruction: systemInstruction,
