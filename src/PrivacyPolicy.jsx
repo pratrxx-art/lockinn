755930462aa0a7495296a71c3e4fd7da97dedeeb
pratrxx-art <pratrxx@gmail.com>
2026-05-16 10:00:02 +0530

    Create PrivacyPolicy.jsx

---
+++ src/PrivacyPolicy.jsx
@@ -0,0 +1,25 @@
+function PrivacyPolicy() {
+  return (
+    <div
+      style={{
+        background: "#0f0f0f",
+        color: "white",
+        minHeight: "100vh",
+        padding: "40px",
+        fontFamily: "Arial"
+      }}
+    >
+      <h1>Privacy Policy</h1>
+
+      <p>
+        This website uses cookies and Google AdSense.
+      </p>
+
+      <p>
+        Google may use cookies to personalize ads.
+      </p>
+    </div>
+  );
+}
+
+export default PrivacyPolicy;
